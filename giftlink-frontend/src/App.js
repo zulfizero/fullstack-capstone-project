765870fc0a5321c854c82846a8fad51de2693a1b
zulfizero <cyedxhah@gmail.com>
2025-06-18 13:46:57 -0400

    7th commit

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,6 +6,7 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 function App() {
   const navigate = useNavigate();
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
         </>
   );
