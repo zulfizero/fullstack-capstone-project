2029404ce46a5e504b11287f4db3d434e44f3fa8
zulfizero <cyedxhah@gmail.com>
2025-06-18 10:48:49 -0400

    2nd commit

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -28,6 +28,7 @@ const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
+const searchRoutes = require('./routes/searchRoutes');
 
 
 
@@ -43,6 +44,7 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
+app.use('/api/search', searchRoutes);
 
 
 // Global Error Handler
